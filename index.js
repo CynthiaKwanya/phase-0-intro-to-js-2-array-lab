9ba6017d71d4d8968d6c2fc7c7cfbc295ba17abd
Cynthia Kwanya Akinyi <kwanyac15@gmail.com>
2023-03-15 14:05:52 +0300

    control flows

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution here!
+function scuberGreetingForFeet(feet) {
+    if (feet <= 400) {
+      return "This one is on me!";
+    } else if (feet > 400 && feet <= 800) {
+      return "I will gladly take your thirty bucks.";
+    } else if (feet > 800 && feet <=1200) {
+      return "you will pay seventy bucks.";
+    } else {
+      return "Please enter a valid distance.";
+    }
+  }
+  
